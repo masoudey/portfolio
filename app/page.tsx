02693056fd184853dff39f3e62c67dbe48f90f31
Masoud Ey <masoud.ey@gmail.com>
2023-07-17 21:14:49 +0330

    fixx

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export const dynamic = 'force-dynamic';
 export const dynamicParams = true;
 export const revalidate = 0;
 
-const Home = async ({ searchParams: { category = '', endcursor } }: Props) => {
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
     const data = (await fetchAllProjects(category, endcursor)) as ProjectSearch;
 
     console.log(data);
