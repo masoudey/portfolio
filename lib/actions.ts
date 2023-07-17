8913a15c730150269cf94836837d529c80a9b366
Masoud Ey <masoud.ey@gmail.com>
2023-07-17 16:06:21 +0330

    testing

--- lib/actions.ts
+++ lib/actions.ts
@@ -13,6 +13,7 @@ import {
 import { ProjectForm } from '@/common.types';
 
 const isProduction = process.env.NODE_ENV === 'production';
+console.log(process.env.NODE_ENV);
 const apiUrl = isProduction
     ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ''
     : 'http://127.0.0.1:4000/graphql';
